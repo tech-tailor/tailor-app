45e1ce19a19e78332d96efa05c1ae7ea73d4fa6e
tech-tailor <ogundareakinniyi8@gmail.com>
2023-08-17 23:55:50 +0100

    updated

--- tailorApp/settings.py
+++ tailorApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 ]
 
 ROOT_URLCONF = 'tailorApp.urls'
