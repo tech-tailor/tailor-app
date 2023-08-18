2fb6a94b5c2b0742e154c6919407e574353a71e0
tech-tailor <ogundareakinniyi8@gmail.com>
2023-08-18 09:10:41 +0100

    update

--- tailorApp/settings.py
+++ tailorApp/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR, / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
