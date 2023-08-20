6de2a76edf73ef39701f204ea912fed35246f2f9
tech-tailor <ogundareakinniyi8@gmail.com>
2023-08-20 16:44:37 +0100

    update

--- tailorApp/settings.py
+++ tailorApp/settings.py
@@ -145,7 +145,7 @@ AWS_S3_REGION_NAME = 'auto'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.r2.cloudflarestorage.com/static/'
+
 
 
 
