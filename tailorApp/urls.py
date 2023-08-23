f7c80664cd786a061e7e3e50710e11a761adf4e9
tech-tailor <ogundareakinniyi8@gmail.com>
2023-08-23 16:16:23 +0100

    update

--- tailorApp/urls.py
+++ tailorApp/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', include('store.urls')),
+    path('work/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
