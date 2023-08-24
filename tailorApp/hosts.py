26fbf63779c9c2ca68f54600b49ff3a2129dd820
tech-tailor <ogundareakinniyi8@gmail.com>
2023-08-24 15:02:35 +0100

    update

--- tailorApp/hosts.py
+++ tailorApp/hosts.py
@@ -4,4 +4,6 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='default'),
     host(r'work', 'core.urls', name='work'),
+    host(r'my-admin', 'admin.site.urls', name='admin'),
+    host(r'clients', 'clients.urls', name='clients'),
 )
