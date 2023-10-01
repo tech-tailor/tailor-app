a46316882308380af2853348cc558faf3423dd05
Akin <ogundareakinniyi8@gmail.com>
2023-10-01 05:51:11 +0100

    settings updated

--- tailorApp/settings.py
+++ tailorApp/settings.py
@@ -189,11 +189,11 @@ else:
 
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DATABASE_NAME'),
-            'USER': 'akin',
-            'PASSWORD':  config('PASSWORD'),
-            'HOST': config('HOST'),
-            'PORT': config('PORT'),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD':  config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
         }, 
     }
 
