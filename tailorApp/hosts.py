eda061dc347f1521613fb7610942d5a718e4d3aa
Akin <ogundareakinniyi8@gmail.com>
2023-10-19 17:53:14 +0100

    tailorApp updated

--- tailorApp/hosts.py
+++ tailorApp/hosts.py
@@ -5,7 +5,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='default'), #for production enviroment
-    host(r'test', 'store.urls', name='test'),   #the home url for testing enviroment
+    host(r'test', settings.ROOT_URLCONF, name='test'),   #the home url for testing enviroment
     host(r'work', 'work.urls', name='work'),   # workapp for production enviroment
     host(r'work-test', 'work.urls', name='work-test'),  # the work app for the testing enviroment
     host(r'gladmin', 'admin_urls', name='admin'), #the admin url for the producton
