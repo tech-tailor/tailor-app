f3026efe2193a7ca80493e49007c72bb0e3fcf28
Akin <ogundareakinniyi8@gmail.com>
2023-11-01 16:08:58 +0100

    update

--- work/import_clients.py
+++ work/import_clients.py
@@ -9,7 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tailorApp.settings") #Replace '
 
 
 print(sys.path)
-project_directory = '/home/tech-tailor/tailorapp/tailorapp-test'
+project_directory = '/home/tech-tailor/python-work/tailor-app'
 sys.path.append(os.path.abspath(project_directory))
 django.setup()
 
@@ -23,54 +23,52 @@ excel_file = 'client_measurement.xlsx'
 sheet_name = 'Sheet1'
 df = pd.read_excel(excel_file, sheet_name)
 
-
-for index, row in df.iterrows():
-    phone_number = row['Phone Number']
-    
-    #try:
-     #   phone_number = to_python(phone_number)
-    #except Exception as e:
-     #   pass
-     #   print(f'invalid phone Number at {index}: {phone_number}, error: {e}')
-     #   continue
+try:
+    for index, row in df.iterrows():
+        phone_number = row['Phone Number']
         
-    client = Clients(
-        measurement_name = row['Client Name'],
-        top_lenght = row['Lenght'],
-        shoulder = row['Shoulder'],
-        #phone_number = phone_number,
-        client_note = row['Client Note'],
-        Round_chest = row['Round Chest'],
-        sleeve_lenght = row['Sleeve Lenght'],
-        neck = row['Neck'],
-        round_arm  = row['Round Arm'],
-        arm_hole = row['Arm Hole'],
-        front_chest = row['Front Chest'],
-        back_chest = row['Back Chest'],
-        cuff = row['Cuff'],
-        short_sleeve_width = row['Short Sleeve Width'],
-        three_quarter_width = row['3Quarter  Width'],
-        long_sleeve_width = row['Long Sleeve Width'],
-        trouser_lenght = row['Long Sleeve Width'],
-        waist = row['Waist'],
-        lap = row['Lap'],
-        knee = row['Knee'],
-        calf = row['Calf'],
-        ankle = row['Ankle'],
-        BR = row['BR'],
-        RBR = row['RRB'],
-        agbada_lenght = row['Agbada Lenght'],
-        agbada_shoulder = row['Agbada Shoulder'],
-        agbada_sleeve = row['Agbada Sleeve'],
-        cap = row['Cap'],
-    )
-    
-    try:
+        #try:
+        #   phone_number = to_python(phone_number)
+        #except Exception as e:
+        #   pass
+        #   print(f'invalid phone Number at {index}: {phone_number}, error: {e}')
+        #   continue
+            
+        client = Clients(
+            measurement_name = row['Client Name'],
+            top_lenght = row['Lenght'],
+            shoulder = row['Shoulder'],
+            #phone_number = phone_number,
+            client_note = row['Client Note'],
+            Round_chest = row['Round Chest'],
+            sleeve_lenght = row['Sleeve Lenght'],
+            neck = row['Neck'],
+            round_arm  = row['Round Arm'],
+            arm_hole = row['Arm Hole'],
+            front_chest = row['Front Chest'],
+            back_chest = row['Back Chest'],
+            cuff = row['Cuff'],
+            short_sleeve_width = row['Short Sleeve Width'],
+            three_quarter_width = row['3Quarter  Width'],
+            long_sleeve_width = row['Long Sleeve Width'],
+            trouser_lenght = row['Long Sleeve Width'],
+            waist = row['Waist'],
+            lap = row['Lap'],
+            knee = row['Knee'],
+            calf = row['Calf'],
+            ankle = row['Ankle'],
+            BR = row['BR'],
+            RBR = row['RRB'],
+            agbada_lenght = row['Agbada Lenght'],
+            agbada_shoulder = row['Agbada Shoulder'],
+            agbada_sleeve = row['Agbada Sleeve'],
+            cap = row['Cap'],
+        )
         #client.full_clean()
         client.save()
         print('client data imported successfully')
-    except ValidationError as e:
-        print(f'validation error at row {index}: {phone_number} {e}')
-        continue
+except Exception as e:
+    print(f'validation error at row {index}: {phone_number} {e}')
+    #continue
         
     
